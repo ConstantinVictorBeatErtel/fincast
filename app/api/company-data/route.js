ad7eeecab363837f4df28c1aa39f846226a8e568
Constantin Ertel <constantinertel18@gmail.com>
2025-06-08 14:28:09 -0700

    Simplify Vercel configuration and add Python requirements

--- app/api/company-data/route.js
+++ app/api/company-data/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-SIMFIN_API_KEY = '1aab9692-30b6-4b82-be79-27d454de3b25';
+const SIMFIN_API_KEY = '1aab9692-30b6-4b82-be79-27d454de3b25';
 
 export const runtime = 'edge';
 
