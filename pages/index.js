d1539c3bce6202b297e1d368fb0ae740554dd1b6
Constantin Ertel <constantinertel18@gmail.com>
2025-06-07 19:02:57 -0700

    Update routing structure with pages/index.js redirect and app/app/page.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
-export { default } from '../app/page'; 
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/app');
+  }, [router]);
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Loading...</h1>
+    </div>
+  );
+} 
